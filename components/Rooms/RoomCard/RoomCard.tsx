0927f45a79ce71a72264aeb2ece94a08791cdb3e
Adele <adelewisdom@gmail.com>
2023-06-05 19:44:33 +0100

    more changes

--- components/Rooms/RoomCard/RoomCard.tsx
+++ components/Rooms/RoomCard/RoomCard.tsx
@@ -19,18 +19,15 @@ const RoomCard = ({ image, name, price, addToCart, id, cart }: RoomCardProps) =>
       item.id === id 
     )
   
- console.log(id, check())
-
-
-
   return (
-    <div onClick={addToCart} title={check() ? "Room is already in cart": ""} id={`${id}`} style={check() ? { filter: "grayscale(1)", cursor: "not-allowed"} : {filter: "grayscale(0)", cursor: "pointer"}} className={s.RoomCard}>
+    <div onClick={addToCart} title={check() ? "Room is already in cart": ""} id={`${id}`} style={check() ? { filter: "grayscale(.7)", cursor: "not-allowed"} : {filter: "grayscale(0)", cursor: "pointer"}} className={s.RoomCard}>
       <Image src={image} alt="" />
       <div>
         <h3>{name}</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quia officiis eveniet, a tempore molestias, numquam magni iusto alias nesciunt, asperiores exercitationem? Harum, ducimus. Itaque recusandae iste hic rerum quia.</p>
         <h4>Cost: ${price} daily</h4>
-        <button style={check() ? { filter: "grayscale(1)", cursor: "not-allowed"} : {filter: "grayscale(0)", cursor: "pointer"}} onClick={addToCart}> Book this room</button>
+        {!check() ? <button style={check() ? { filter: "grayscale(1)", cursor: "not-allowed"} : {filter: "grayscale(0)", cursor: "pointer"}} onClick={addToCart}> Book this room</button> : <strong><small> Room has been booked</small> </strong> }
+        
        </div>
     </div>
   )
